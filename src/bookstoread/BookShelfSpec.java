3b0c1bd412e0ea9831e497a22973e13190d5f795
yankey <nwolle14@gmail.com>
2026-05-07 19:51:07 +0000

    refactorisation du code

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BookShelfSpec {
+public class BookShelfSpec{
 
     private BookShelf shelf;
 
